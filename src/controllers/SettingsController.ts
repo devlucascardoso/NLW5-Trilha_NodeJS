4b3d29471fb51e6edfb8a3d47a7c2202dfddbd57
Lucas Cardoso <83316119+devlucascardoso@users.noreply.github.com>
2021-05-20 00:02:51 -0300

    Update SettingsController.ts

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,10 +1,9 @@
-import { Request, Reponse } from "express";
+import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 class SettingsController {
-
-  async create(request, reponse) {
+  async create(request: Request, response: Response) {
     const { chat, username } = request.body;
 
   const settingsRepository = getCustomRepository(SettingsRepository);
@@ -14,7 +13,7 @@ class SettingsController {
     username,
   });
   
-  await SettingsRepository.save(settings);
+  await settingsRepository.save(settings);
 
   return response.json(settings);
   }
